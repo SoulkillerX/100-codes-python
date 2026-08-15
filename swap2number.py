6218ac43618708b81562c0ff7ab163c5b48b5be2
SoulkillerX <saksham.kashyap.2007.29@gmail.com>
2026-08-15 11:43:07 +0530

    Day 3

---
+++ swap2number.py
@@ -0,0 +1,10 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Before swapping: a =", a, ", b =", b)
+
+c = a
+a = b
+b = c
+
+print("After swapping: a =", a, ", b =", b)
