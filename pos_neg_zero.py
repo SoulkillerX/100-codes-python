0bf61684b0efa5301a32adaa85076255c3ba4b56
SoulkillerX <saksham.kashyap.2007.29@gmail.com>
2026-08-17 21:54:15 +0530

    Day 6

---
+++ pos_neg_zero.py
@@ -0,0 +1,9 @@
+num = int(input("Enter an integer: "))
+
+if num > 0:
+    print(num, "is Positive")
+else:
+    if num < 0:
+        print(num, "is Negative")
+    else:
+        print(num, "is Zero")
